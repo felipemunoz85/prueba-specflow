a4cd5a6ac3f4b6e2846893a21ab496b5d122a875
Felipe Muñoz <luis.f.munoz1985@gmail.com>
2018-11-10 08:43:35 -0500

    First commit

---
+++ Calculator.feature
@@ -0,0 +1,29 @@
+﻿Feature: Calculator
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+@mytag
+Scenario: Add two numbers
+	Given I have entered 50 into the calculator to add
+	And I have also entered 70 into the calculator to add
+	When I press add
+	Then the result should be 120 on the screen when adding
+
+Scenario: Substract two numbers
+	Given I have entered 70 into the calculator to be substracted
+	And I have also entered 20 into the calculator to substract
+	When I press substract
+	Then the result should be 50 on the screen when substracting
+
+Scenario: Multiply two numbers
+	Given I have entered 70 into the calculator to multiply
+	And I have also entered 25 into the calculator to multiply
+	When I press multiply
+	Then the result should be 1750 on the screen when multiplying
+
+Scenario: Divide two numbers
+	Given I have entered 960 into the calculator to be divided
+	And I have also entered 20 into the calculator to divide
+	When I press divide
+	Then the result should be 48 on the screen when dividing
---
+++ CalculatorSteps.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ejercicio
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int result;
+        private Calculator.Calculator calculator = new Calculator.Calculator();
+        [Given(@"I have entered (.*) into the calculator to add")]
+        public void GivenIHaveEnteredIntoTheCalculatorToAdd(int term1)
+        {
+            calculator.FirstNumber = term1;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator to add")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculatorToAdd(int term2)
+        {
+            calculator.SecondNumber = term2;
+        }
+        
+        [Given(@"I have entered (.*) into the calculator to be substracted")]
+        public void GivenIHaveEnteredIntoTheCalculatorToBeSubstracted(int term1)
+        {
+            calculator.FirstNumber = term1;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator to substract")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculatorToSubstract(int term2)
+        {
+            calculator.SecondNumber = term2;
+        }
+        
+        [Given(@"I have entered (.*) into the calculator to multiply")]
+        public void GivenIHaveEnteredIntoTheCalculatorToMultiply(int term1)
+        {
+            calculator.FirstNumber = term1;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator to multiply")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculatorToMultiply(int term2)
+        {
+            calculator.SecondNumber = term2;
+        }
+        
+        [Given(@"I have entered (.*) into the calculator to be divided")]
+        public void GivenIHaveEnteredIntoTheCalculatorToBeDivided(int term1)
+        {
+            calculator.FirstNumber = term1;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator to divide")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculatorToDivide(int term2)
+        {
+            calculator.SecondNumber = term2;
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+        
+        [When(@"I press substract")]
+        public void WhenIPressSubstract()
+        {
+            result = calculator.Substract();
+        }
+        
+        [When(@"I press multiply")]
+        public void WhenIPressMultiply()
+        {
+            result = calculator.Multiply();
+        }
+        
+        [When(@"I press divide")]
+        public void WhenIPressDivide()
+        {
+            result = calculator.Divide();
+        }
+        
+        [Then(@"the result should be (.*) on the screen when adding")]
+        public void ThenTheResultShouldBeOnTheScreenWhenAdding(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [Then(@"the result should be (.*) on the screen when substracting")]
+        public void ThenTheResultShouldBeOnTheScreenWhenSubstracting(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [Then(@"the result should be (.*) on the screen when multiplying")]
+        public void ThenTheResultShouldBeOnTheScreenWhenMultiplying(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [Then(@"the result should be (.*) on the screen when dividing")]
+        public void ThenTheResultShouldBeOnTheScreenWhenDividing(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
